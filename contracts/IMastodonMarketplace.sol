e94939034a13b2720b2b3cc1e5e4576bc2784d80
CatalinBalut <balut.catalin@yahoo.com>
2023-10-27 13:46:26 +0300

    marketplace interface

---
+++ contracts/IMastodonMarketplace.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import {IERC721} from '@openzeppelin/contracts/interfaces/IERC721.sol';
+
+interface IMastodonMarketplace {
+
+    enum PayoutToken{
+        NativeToken,
+        Xen,
+        Dxn
+    }
+
+    struct Order {
+        IERC721 nftContract;
+        address seller;
+        uint256 tokenId;
+        address payoutToken;
+        uint256 price;
+    }
+
+    struct InputOrder{
+        IERC721 nftContract;
+        uint256 tokenId;
+        address payoutToken;
+        uint256 price;
+    }
+
+    event List(uint256, Order);
+
+    event Delist();
+
+    event Buy();
+
+    function list(InputOrder memory order) external;
+
+    function delist() external;
+
+    function buy() external;
+
+    //Optional
+
+    // function batchList() external;
+
+    // function batchDelist() external;
+
+    // function batchBuy() external;
+
+    // function changePrice() external;
+
+}
