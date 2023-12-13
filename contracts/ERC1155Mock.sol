e26ba3e626866dcc392b007625d22c43c192c3b4
CatalinBalut <balut.catalin@yahoo.com>
2023-12-13 20:27:23 +0200

    modified ERC1155Mock to be dynamic

--- contracts/ERC1155Mock.sol
+++ contracts/ERC1155Mock.sol
@@ -6,24 +6,9 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 contract ERC1155Mock is ERC1155 {
     constructor() ERC1155("//uri") {}
 
-    enum NFTType {
-        GOLD,
-        SILVER,
-        BRONZE
-    }
-
     // Mint a specified amount of gold NFTs
-    function mintGold(address recipient, uint256 amount) public {
-        _mint(recipient, uint256(NFTType.GOLD), amount, "");
+    function mint(address recipient, uint256 id,  uint256 value) public {
+        _mint(recipient, id, value, "");
     }
 
-    // Mint a specified amount of silver NFTs
-    function mintSilver(address recipient, uint256 amount) public {
-        _mint(recipient, uint256(NFTType.SILVER), amount, "");
-    }
-
-    // Mint a specified amount of bronze NFTs
-    function mintBronze(address recipient, uint256 amount) public {
-        _mint(recipient, uint256(NFTType.BRONZE), amount, "");
-    }
 }
