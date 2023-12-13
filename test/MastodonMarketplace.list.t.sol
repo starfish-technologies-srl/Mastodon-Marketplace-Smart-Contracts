492f72869d2950bc96bb6922697c29f4754dc9a1
CatalinBalut <balut.catalin@yahoo.com>
2023-12-13 21:12:13 +0200

    first tests for list delist and buy for mock smart contracts

---
+++ test/MastodonMarketplace.list.t.sol
@@ -0,0 +1,155 @@
+pragma solidity 0.8.21;
+
+import "forge-std/Test.sol"; //forge standard library
+import "forge-std/console.sol";
+
+import {MastodonMarketplace} from "../contracts/MastodonMarketplace.sol";
+
+import {BurnerMock} from "../contracts/BurnerMock.sol";
+import {ERC20Mock} from "../contracts/ERC20Mock.sol";
+import {ERC721Mock} from "../contracts/ERC721Mock.sol";
+import {ERC1155Mock} from "../contracts/ERC1155Mock.sol";
+
+import {IMastodonMarketplace} from "../contracts/IMastodonMarketplace.sol";
+
+//0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84 the general deployer
+
+contract List is Test {
+    MastodonMarketplace mastodonMarketplace;
+    ERC20Mock erc20MockA;
+    ERC20Mock erc20MockB;
+    ERC721Mock erc721Mock;
+    ERC1155Mock erc1155Mock;
+    BurnerMock burnerMock;
+
+    address add1 = address(1);
+    InputOrderERC721 inputOrderERC721;
+    struct InputOrderERC721 {
+        address nftContract;
+        uint256 tokenId;
+        address payoutToken;
+        uint256 price;
+    }
+
+    // IMastodonMarketplace iMM;
+
+    // struct InputOrder{
+    //     address nftContract;
+    //     uint256 tokenId;
+    //     uint256 supply;
+    //     address payoutToken;
+    //     uint256 price;
+    // }
+    // InputOrder imms;
+
+    function setUp() public {
+        //setUp: An optional function invoked before each test case is run.
+        erc20MockA = new ERC20Mock(); //xen
+        erc20MockB = new ERC20Mock();
+        erc721Mock = new ERC721Mock();
+        erc1155Mock = new ERC1155Mock();
+        burnerMock = new BurnerMock(erc20MockA);
+        // iMM = new IMastodonMarketplace();
+        mastodonMarketplace = new MastodonMarketplace(
+            erc20MockA,
+            erc20MockB,
+            address(burnerMock)
+        );
+    }
+
+    function test_ListMockERC721() public {
+        address input_nftContract = address(erc721Mock);
+        address input_seller = add1;
+        uint256 input_tokenId = 1;
+        uint256 input_supply = 0; //always 0 for ERC721
+        address input_payoutToken = add1;
+        uint256 input_price = 1;
+
+        IMastodonMarketplace.InputOrder[]
+            memory inputOrders = new IMastodonMarketplace.InputOrder[](1);
+        inputOrders[0] = IMastodonMarketplace.InputOrder(
+            input_nftContract,
+            input_tokenId,
+            input_supply,
+            input_payoutToken,
+            input_price
+        );
+
+        erc721Mock.mint(add1);
+        vm.prank(add1);
+        erc721Mock.approve(address(mastodonMarketplace), 1);
+        vm.prank(add1);
+        mastodonMarketplace.batchList(inputOrders);
+
+        (
+            address nftContract,
+            address seller,
+            uint256 tokenId,
+            uint256 supply,
+            address payoutToken,
+            uint256 price
+        ) = mastodonMarketplace.orders(1);
+
+        assertEq(nftContract, address(erc721Mock));
+        assertEq(seller, input_seller);
+        assertEq(tokenId, input_tokenId);
+        assertEq(supply, input_supply);
+        assertEq(payoutToken, input_payoutToken);
+        assertEq(price, input_price);
+    }
+
+    function test_ListMockERC1155() public {
+        address input_nftContract = address(erc1155Mock);
+        address input_seller = add1;
+        uint256 input_tokenId = 1;
+        uint256 input_supply = 10;
+        address input_payoutToken = add1;
+        uint256 input_price = 1;
+
+        IMastodonMarketplace.InputOrder[]
+            memory inputOrders = new IMastodonMarketplace.InputOrder[](1);
+        inputOrders[0] = IMastodonMarketplace.InputOrder(
+            input_nftContract,
+            input_tokenId,
+            input_supply,
+            input_payoutToken,
+            input_price
+        );
+
+        erc1155Mock.mint(add1, input_tokenId, input_supply);
+        vm.prank(add1);
+        erc1155Mock.setApprovalForAll(address(mastodonMarketplace), true);
+        vm.prank(add1);
+        mastodonMarketplace.batchList(inputOrders);
+
+        (
+            address nftContract,
+            address seller,
+            uint256 tokenId,
+            uint256 supply,
+            address payoutToken,
+            uint256 price
+        ) = mastodonMarketplace.orders(1);
+
+        assertEq(nftContract, address(erc1155Mock));
+        assertEq(seller, input_seller);
+        assertEq(tokenId, input_tokenId);
+        assertEq(supply, input_supply);
+        assertEq(payoutToken, input_payoutToken);
+        assertEq(price, input_price);
+    }
+
+    function test_ListXENFT() public {}
+
+    function test_ListDBXENFT() public {}
+
+    // function testFail_Subtract43() public { //testFail: The inverse of the test prefix - if the function does not revert, the test fails.
+    //     testNumber -= 43;
+    // }
+}
+
+contract ErrorsTest {
+    function arithmeticError(uint256 a) public {
+        uint256 a = a - 100;
+    }
+}
